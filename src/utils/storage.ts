a83537c1ebb95014d151152801fbcdeee9ce216c
Candiepih <mutheeal.am@gmail.com>
2022-02-18 18:42:40 +0300

    added methods for storage actions

---
+++ src/utils/storage.ts
@@ -0,0 +1,37 @@
+/**
+ * @class Storage
+ * Storage class for storing and retrieving data
+ * @author Alex Steve
+ * @property {chrome.storage.SyncStorageArea} _storageKey - The key to use for the storage
+ */
+class Storage {
+  private _storageKey: chrome.storage.SyncStorageArea;
+
+  constructor() {
+    this._storageKey = chrome.storage.sync;
+  }
+
+  /**
+   * @method save
+   * @description Sets a value in storage
+   * @param {String} key The key to set
+   * @param {String} value The value to set
+   * @returns The value stored at the key or undefined if the key does not exist
+   */
+  public save(key: string, value: unknown): boolean {
+    this._storageKey.set({ [key]: value }).catch(() => false);
+    return true;
+  }
+
+  /**
+   * @method get
+   * @description Gets a value from storage
+   * @param {String} key The key to get
+   * @returns The value stored at the key or undefined if the key does not exist
+   */
+  public async get(key: string): Promise<unknown> {
+    const result: unknown = await this._storageKey.get(key);
+    return result;
+  }
+}
+export default Storage;
