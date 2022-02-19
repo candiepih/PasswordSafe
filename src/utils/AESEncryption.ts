08fb086ad15b92af5ca57080d28ad4547b9ce1ab
Candiepih <mutheeal.am@gmail.com>
2022-02-19 20:54:51 +0300

    Made methods static

--- src/utils/AESEncryption.ts
+++ src/utils/AESEncryption.ts
@@ -1,15 +1,12 @@
 import { AES, enc, SHA256 } from 'crypto-ts';
+import * as crypto from "crypto";
 
 /**
  * Encrypts a string using AES-256-CBC
  * @class AESEncryption
  * @param {string} key - The key to use for encryption
  */
-class AESEncryption {
-  constructor(private key: string) {
-    this.key = SHA256(key).toString();
-  }
-
+class AESencryption {
   /**
    * @method encrypt
    * Encrypts a string using AES-256-CBC
@@ -17,13 +14,13 @@ class AESEncryption {
    * @returns The encrypted text
    * @throws Error if the text is not a string
    */
-  encrypt(text: string): string {
+  public static encrypt(text: string, key: string): string {
     if (typeof text === 'object') {
       text = JSON.stringify(text);
     } else if (typeof text !== 'string') {
       throw new Error('The text to encrypt must be a string or an object');
     }
-    const cipherText: string = AES.encrypt(text, this.key).toString();
+    const cipherText: string = AES.encrypt(text, key).toString();
     return cipherText;
   }
 
@@ -34,13 +31,24 @@ class AESEncryption {
    * @returns The decrypted text
    * @throws Error if the text is not a string
    */
-  decrypt(text: string): string {
+  public static decrypt(text: string, key: string): string {
     if (typeof text !== 'string') {
       throw new Error('The text to decrypt must be a string');
     }
-    const plainText: string = AES.decrypt(text, this.key).toString(enc.Utf8);
+    const plainText: string = AES.decrypt(text, key).toString(enc.Utf8);
     return plainText;
+  }
+
+  /**
+   * @method generateKey
+   * @description Generates a key for encryption
+   * @returns {string} The generated key
+   * @throws Error if the key is not a string
+   */
+  public static encryptSecretKey(key: string): string {
+    const encKey = crypto.createHash('sha256').update(key).digest('hex');
+    return encKey;
   }
 }
 
-export default AESEncryption;
+export default AESencryption;
